c05770a8de682cfd13783f566a3761cbb46feccb
ngranzow <nate.granzow@gmail.com>
2022-03-21 20:39:34 -0500

    created comments routes and models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -10,7 +11,15 @@ User.hasMany(Vote, {
     foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
 Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
 
@@ -25,6 +34,14 @@ Vote.belongsTo(Post, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_ic'
+});
+
 User.belongsToMany(Post, {
     through: Vote,
     as: 'voted_posts',
@@ -37,4 +54,4 @@ Post.belongsToMany(User, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
